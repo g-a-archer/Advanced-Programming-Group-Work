d45aefa29bf8f7595a95a3b4a47d0cd3921ada86
g-a-archer <79874798+g-a-archer@users.noreply.github.com>
2022-10-20 08:10:58 -0500

    Update CheckOut.java

--- View/CheckOut.java
+++ View/CheckOut.java
@@ -1,30 +1,17 @@
-import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Random;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
-public class CheckOut  extends JFrame implements ActionListener{
+public class CheckOut {
 
 
-	private static final long serialVersionUID = 1L;
-	private JFrame frame;
-	private JPanel headerPanel;
-	private JPanel invoicePanel;
-	private JPanel datePanel;
-	private JPanel customerPanel;
-	private JPanel itemPanel;
-	private JPanel memberPanel;
-	private JPanel discountPanel;
-	private JPanel recievedPanel;
 	private JLabel headerLabel;
 	private JLabel invoiceLabel;
 	private JLabel dateLabel;
@@ -39,6 +26,7 @@ public class CheckOut  extends JFrame implements ActionListener{
 	private JLabel totalLabel;
 	private JLabel recievedLabel;
 	private JLabel changeLabel;
+	private JFrame frame;
 	private JTextField invoiceTextField;
 	private JTextField customerTextField;
 	private JTextField idTextField;
@@ -47,133 +35,254 @@ public class CheckOut  extends JFrame implements ActionListener{
 	private JTextField recievedTextField;
 	private JRadioButton yesMemberRadioButton;
 	private JRadioButton noMemberRadioButton;
-	private Random random;
-	private String randomString;
+	private GridBagConstraints gbc;
+	private JButton button;
+	private JLabel navigationLabel;
+	private JComboBox<String> combobox;
 	public CheckOut() {
-		frame =new JFrame();
-		
-	
-		headerLabel=new JLabel("Check Out");
-		
-		headerPanel=new JPanel(new FlowLayout(FlowLayout.CENTER));
+   frame=new JFrame();
+
+    navigationLabel=new JLabel("Navigation:");
+
+
+button=new JButton("Logout");
+String navigation[]= {" ","Dashboard","Customer Database","Staff Database","Stock and Inventory","Check Out","Sales Reports","Register Customer"};
+ combobox=new JComboBox<>(navigation);
+ 
+        headerLabel=new JLabel("Check Out");
 		invoiceLabel=new JLabel("Invoice#:");
+		//use random number to set invoice number
 		invoiceTextField= new JTextField(50);
-		invoicePanel=new JPanel(new FlowLayout(FlowLayout.LEFT));
-		random=new Random();
-		
-		invoiceTextField.setText(randomString);
-		datePanel= new JPanel(new FlowLayout(FlowLayout.LEFT));
-		 dateLabel=new JLabel("Billing Date");
-		
-		//change header to this then add buttons to header
-		//namePanel=new JPanel(new FlowLayout(FlowLayout.CENTER);
-		//JLabel nameLabel=new JLabel();
-		
+		dateLabel=new JLabel("Billing Date");
 		cashierLabel=new JLabel("Cashier");
-		
-		 customerPanel=new JPanel(new FlowLayout(FlowLayout.LEFT));
-		 customerLabel=new JLabel("Customer");
-		 customerTextField= new JTextField(50);
-		 
+		customerLabel=new JLabel("Customer");
+		customerTextField= new JTextField(50);
 		idLabel=new JLabel("Customer ID");
 		idTextField= new JTextField(20); 
-		
-		itemPanel=new JPanel(new FlowLayout(FlowLayout.LEFT));
-		 itemLabel=new JLabel("Item");
+		itemLabel=new JLabel("Item");
 		itemTextField= new JTextField(50);
-		 
 		quantityLabel=new JLabel("Quantity of Item");
 		quantityTextField= new JTextField(50);
-		
-		memberPanel=new JPanel(new FlowLayout(FlowLayout.LEFT));
-		 memberLabel=new JLabel("Member");
+		memberLabel=new JLabel("Member");
 		yesMemberRadioButton=new JRadioButton("Yes");
 		yesMemberRadioButton.setBounds(100,50,100,30);
 		noMemberRadioButton=new JRadioButton("No");
 		noMemberRadioButton.setBounds(100,100,100,30);
-		 discountPanel=new JPanel(new FlowLayout(FlowLayout.LEFT));
-		 discountLabel=new JLabel("Discount");
+		discountLabel=new JLabel("Discount");
 		
 		taxLabel=new JLabel("Tax");
 		
 		 totalLabel=new JLabel("Total");
 		
-		 recievedPanel=new JPanel(new FlowLayout(FlowLayout.LEFT));
 		 recievedLabel=new JLabel("Amount Recieved:");
-		recievedTextField=new JTextField();
-		recievedTextField.setSize(100,50);
+		 recievedTextField=new JTextField();
 		 changeLabel=new JLabel("Change");
-		 
-		
-		frame.setVisible(true);
 		layout();
 		
 	
 	}
 	public void layout() {
-		frame.setLayout(new GridLayout(7,0,0,0));
-		
-		headerPanel.setSize(new Dimension(100,200));
-		headerPanel.add(headerLabel);
-		frame.add(headerPanel);
-		
-		invoicePanel.setSize(new Dimension(100,100));
-		invoicePanel.add(invoiceLabel);
-		invoicePanel.add(invoiceTextField);
-		frame.add(invoicePanel);
-		
-		datePanel.setSize(new Dimension(100,30));
-		datePanel.add(dateLabel);
-		datePanel.add(cashierLabel);
-		frame.add(datePanel);
-	
-		customerPanel.setSize(new Dimension(100,30));
-		customerPanel.add(customerLabel);
-		customerPanel.add(customerTextField);
-		customerPanel.add(idLabel);
-		customerPanel.add(idTextField);
-		frame.add(customerPanel);
-		
-		itemPanel.setSize(new Dimension(100,30));
-		itemPanel.add(itemLabel);
-		itemTextField.add(idTextField);
-		itemPanel.add(quantityLabel);
-		itemPanel.add(quantityTextField);
-		frame.add(itemPanel);
-		
-		memberPanel.setSize(new Dimension(100,30));
-		memberPanel.add(memberLabel);
-		memberPanel.add(yesMemberRadioButton);
-		memberPanel.add(noMemberRadioButton);
-		frame.add(memberPanel);
-		
-		discountPanel.setSize(new Dimension(100,30));
-		discountPanel.add(discountLabel);
-		discountPanel.add(taxLabel);
-		discountPanel.add(totalLabel);
-		frame.add(discountPanel);
-		
-		recievedPanel.setSize(new Dimension(100,30));
-		recievedPanel.add(recievedLabel);
-		recievedPanel.add(recievedTextField);
-		recievedPanel.add(changeLabel);
-		frame.add(recievedPanel);
-		
-		
-		frame.setSize(new Dimension(600,800));
+		frame.setLayout(new GridBagLayout());
+		gbc=new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 50;
+		gbc.ipady = 20;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		frame.add(navigationLabel,gbc);
+
+		gbc=new GridBagConstraints();
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 20;
+		gbc.ipady = 5;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		frame.add(combobox,gbc);
+
+		gbc = new GridBagConstraints();
+		gbc.gridx = 4;
+		gbc.gridy = 0;
+		gbc.gridwidth = 1;
+		gbc.ipadx = 20;
+		gbc.ipady = 10;
+		gbc.anchor = GridBagConstraints.EAST;
+		frame.add(button,gbc);
+		gbc=new GridBagConstraints();
+		gbc.gridx = 3;
+		gbc.gridy = 1;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 50;
+		gbc.ipady = 20;
+		gbc.anchor = GridBagConstraints.EAST;
+		frame.add(headerLabel,gbc);
+		gbc=new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 50;
+		gbc.ipady = 20;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(invoiceLabel,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 50;
+		gbc.ipady = 20;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(dateLabel,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 4;
+		gbc.gridy = 3;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 50;
+		gbc.ipady = 20;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(cashierLabel,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 50;
+		gbc.ipady = 20;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(customerLabel,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 1;
+		gbc.gridy = 4;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 150;
+		gbc.ipady = 5;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(customerTextField,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 4;
+		gbc.gridy = 4;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 50;
+		gbc.ipady = 20;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(idLabel,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 5;
+		gbc.gridy = 4;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 150;
+		gbc.ipady = 5;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(idTextField,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 5;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 50;
+		gbc.ipady = 20;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(itemLabel,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 1;
+		gbc.gridy = 5;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 150;
+		gbc.ipady = 5;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(itemTextField,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 4;
+		gbc.gridy = 5;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 50;
+		gbc.ipady = 20;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(quantityLabel,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 5;
+		gbc.gridy = 5;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 150;
+		gbc.ipady = 5;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(quantityTextField,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 6;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 50;
+		gbc.ipady = 20;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(memberLabel,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 1;
+		gbc.gridy = 6;
+		gbc.gridwidth = 1; 
+        frame.add(yesMemberRadioButton,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 2;
+		gbc.gridy = 6;
+		gbc.gridwidth = 1; 
+        frame.add(noMemberRadioButton,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 7;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 50;
+		gbc.ipady = 20;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(discountLabel,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 2;
+		gbc.gridy = 7;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 50;
+		gbc.ipady = 20;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(taxLabel,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 4;
+		gbc.gridy = 7;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 50;
+		gbc.ipady = 20;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(totalLabel,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 8;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 50;
+		gbc.ipady = 20;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(recievedLabel,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 1;
+		gbc.gridy = 8;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 150;
+		gbc.ipady = 5;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(recievedTextField,gbc);
+        gbc=new GridBagConstraints();
+		gbc.gridx = 4;
+		gbc.gridy = 8;
+		gbc.gridwidth = 1; 
+		gbc.ipadx = 50;
+		gbc.ipady = 20;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(changeLabel,gbc);
+       
+		frame.setSize(new Dimension(800,500));
 		frame.setVisible(true);
 		frame.setResizable(false);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		random.setSeed(1234567890);
-		randomString=String.valueOf(random);
 		
 	}
+	
+	
 	public static void main(String[] args) {
 		new CheckOut();
 	}
 }
+
