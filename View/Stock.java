32f397bb309b65e38d5e73668c6876f5e55ba253
g-a-archer <79874798+g-a-archer@users.noreply.github.com>
2022-10-24 18:24:48 -0500

    Update Stock.java

--- View/Stock.java
+++ View/Stock.java
@@ -33,7 +33,7 @@ public class Stock{
 	private JPanel panel6;
 	private JPanel borderPanel;
 	private String headings[]= {"Product Code","Name","Short Description","Long Description","Items in Stock","Unit Price"};
-	private int row=15;
+	private int row=30;
 	private JButton updateButton;
 	private JButton deleteButton;
 	private JButton insertButton;
@@ -46,6 +46,7 @@ public class Stock{
 		 String navigation[]= {" ","Dashboard","Customer Database","Staff Database","Stock and Inventory","Check Out","Sales Reports","Register Customer"};
 		 combobox=new JComboBox<>(navigation);
 		 headerLabel=new JLabel("Stock and Inventory");
+		//headerLabel.setBounds(100,50,50,40);
 		 DefaultTableModel model = new DefaultTableModel(row, headings.length) ;
 		 model.setColumnIdentifiers(headings);
 		 table=new JTable(model);
@@ -55,19 +56,25 @@ public class Stock{
 		 insertButton=new JButton("Insert");
 		 panel1=new JPanel(new FlowLayout(FlowLayout.LEFT));
 		 panel2=new JPanel(new FlowLayout(FlowLayout.RIGHT));
-		 panel3=new JPanel(new FlowLayout(FlowLayout.CENTER));
+		// panel3=new JPanel(new FlowLayout(FlowLayout.CENTER));
+		panel3=new JPanel();
 		 panel4=new JPanel(new BorderLayout(0,0));
+		 
 		 panel5=new JPanel(new BorderLayout(0,0));
-		 panel5.setSize(400,500);
-		 panel6=new JPanel();
-		 borderPanel=new JPanel();
+		// panel5.setSize(100,400);
+		 panel6=new JPanel(new FlowLayout());
+		 borderPanel=new JPanel(new BorderLayout(0,0));
 		 borderPanel.setBorder(border);
-		
+		 
+		 //GridLayout layout = new GridLayout(0,3);
+		 //layout.setHgap(10);
+	      //layout.setVgap(10);
 		layout();
 	}
 	
 	public void layout() {
-		frame.setLayout(new GridLayout(0,1,1,2));
+		frame.setLayout(new BorderLayout(3,1));
+		
 		panel1.add(navigationLabel);
 		panel1.add(combobox);
 		//frame.add(panel1);
@@ -75,23 +82,25 @@ public class Stock{
 		//frame.add(panel2);
 		panel4.add(panel1,BorderLayout.WEST);
 		panel4.add(panel2,BorderLayout.EAST);
-		frame.add(panel4);
-		panel3.add(headerLabel,BorderLayout.CENTER);
-		frame.add(panel3);
+		frame.add(panel4,BorderLayout.PAGE_START);
+		panel3.add(headerLabel);
+		//panel3.setHgap(10);
+	    //panel3.setVgap(10);
+		frame.add(panel3,BorderLayout.AFTER_LAST_LINE);
 		panel5.add(table.getTableHeader(),BorderLayout.NORTH);
 		panel5.add(table);
 		frame.add(panel5);
 		panel6.add(updateButton);
 		panel6.add(deleteButton);
 		panel6.add(insertButton);
-		frame.add(panel6);
-		borderPanel.setLayout(new GridLayout(0,1,20,0));
+		//frame.add(panel6);
+		borderPanel.add(panel3,BorderLayout.PAGE_START);
 		borderPanel.add(panel5);
-		borderPanel.add(panel6);
-		
+		borderPanel.add(panel6,BorderLayout.PAGE_END);
+		//frame.add(borderPanel);
 		//borderPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-		frame.getContentPane().add(borderPanel,BorderLayout.CENTER);
-		frame.setSize(new Dimension(800,500));
+		frame.getContentPane().add(borderPanel);
+		frame.setSize(new Dimension(400,600));
 		frame.setVisible(true);
 		frame.setResizable(true);
 		
