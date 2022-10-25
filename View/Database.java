046d4a9035dda01b8bf5828ad525d5622360c99f
g-a-archer <79874798+g-a-archer@users.noreply.github.com>
2022-10-24 20:06:02 -0500

    Update Database.java

--- View/Database.java
+++ View/Database.java
@@ -10,6 +10,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class Database extends JFrame implements ActionListener {
 
@@ -21,6 +23,9 @@ public class Database extends JFrame implements ActionListener {
 	private JPanel management;
 	private JPanel inventory;
 	private JPanel accounting;
+	private JTable managementTable;
+	private JTable inventoryTable;
+	private JTable accountingTable;
 	private JLabel navigationLabel;
 	private JPanel panel1;
 	private JPanel panel2;
@@ -30,12 +35,14 @@ public class Database extends JFrame implements ActionListener {
 	private JComboBox<String> combobox;
 	private JButton button;
 	private JLabel headerLabel;
+	private String headings[]= {"Department Code","Name"};
+	private int row =35;
 	Database(){
 		frame= new JFrame();
 		dashboard=new JPanel();
-		management=new JPanel();
-		inventory=new JPanel();
-		accounting=new JPanel();
+		management=new JPanel(new BorderLayout(0,0));
+		inventory=new JPanel(new BorderLayout(0,0));
+		accounting=new JPanel(new BorderLayout(0,0));
 		tab=new JTabbedPane();
 		tab.setBounds(500, 500, 700,400);
 		//navigationPanel =new JPanel();
@@ -51,10 +58,17 @@ public class Database extends JFrame implements ActionListener {
 		 panel3=new JPanel(new FlowLayout(FlowLayout.CENTER));
 		 panel4=new JPanel(new BorderLayout(0,0));
 		 panel5=new JPanel(new BorderLayout(1,0));
+		 DefaultTableModel model = new DefaultTableModel(row, headings.length) ;
+		 model.setColumnIdentifiers(headings);
+		 
+		 managementTable=new JTable(model);
+		
+		 inventoryTable=new JTable(model);
+		 accountingTable=new JTable(model);
 		layout();
 	}
 	public void layout() {
-		frame.setLayout(new BorderLayout(2,1));
+		frame.setLayout(new BorderLayout(0,1));
 	
 		panel1.add(navigationLabel);
 		panel1.add(combobox);
@@ -68,13 +82,25 @@ public class Database extends JFrame implements ActionListener {
 		
 		//frame.add(navigationPanel);
 		tab.add("Dashboard",dashboard);
+		 managementTable.getColumnModel().getColumn(0).setPreferredWidth(50);
+		 managementTable.getColumnModel().getColumn(1).setPreferredWidth(450);
+		management.add(managementTable.getTableHeader(),BorderLayout.PAGE_START);
+		management.add(managementTable,BorderLayout.CENTER);
 		tab.add("Management",management);
+		 inventoryTable.getColumnModel().getColumn(0).setPreferredWidth(50);
+		 inventoryTable.getColumnModel().getColumn(1).setPreferredWidth(450);
+		inventory.add(inventoryTable.getTableHeader(),BorderLayout.PAGE_START);
+		inventory.add(inventoryTable,BorderLayout.CENTER);
 		tab.add("Inventory",inventory);
+		 accountingTable.getColumnModel().getColumn(0).setPreferredWidth(50);
+		 accountingTable.getColumnModel().getColumn(1).setPreferredWidth(450);
+		accounting.add(accountingTable.getTableHeader(),BorderLayout.PAGE_START);
+		accounting.add(accountingTable,BorderLayout.CENTER);
 		tab.add("Accounting and Sales",accounting);
 		panel5.add(panel3,BorderLayout.NORTH);
 		panel5.add(tab);
 		//panel5.setSize(400,200);
-		frame.add(panel5,BorderLayout.CENTER);
+		frame.getContentPane().add(panel5,BorderLayout.CENTER);
 		frame.setSize(700,700);
 		frame.setVisible(true);
 		//frame.isResizable();
